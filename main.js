0fde0520ceff24f1f61d25edc3e398563d92317c
unknown <rakibulhasan.seu.cse@gmail.com>
2021-09-02 13:42:11 +0600

    More bugs to be fixed

--- main.js
+++ main.js
@@ -1,31 +1,66 @@
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('search-btn');
+const detailsContainer = document.getElementById('books-details');
 const countContainer = document.getElementById('count');
+const errorDiv = document.getElementById('error-div');
+const spinner = document.getElementById("spinner");
+let count = 0;
 
 searchBtn.addEventListener('click', function(){
     const searchText = searchInput.value;
+    //check input is null or not
+    if (searchText === "") {
+        errorDiv.innerText = "Search field cannot be empty!!!";
+        return;
+      }
+    // clear details
+    count = 0;
+    detailsContainer.innerHTML = "";
+    
+
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
-      fetch(url)
-      .then(res => res.json())
-      .then(data => showInfo(data));
+    spinner.classList.remove("d-none");
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+        // Setting a timer of 1.5s, before removing the spinnner, and showing data
+        setTimeout(() => {
+            spinner.classList.add("d-none");
+            showInfo(data);
+          }, 0);
+        })
 });
 
-const showInfo = info =>{
-    let count = 0;
+const showInfo = info => {
     const convertedArray = info.docs;
-    convertedArray.forEach(details => {
-        const detailsContainer = document.getElementById('books-details');
-        const rowDiv = document.createElement('div');
-        rowDiv.innerHTML =  `
-        <div class="row col-md-4 my-2 py-2 border border-dark">
-            <p>Book Name: ${details.title}</p>
-            <p>Author Name: ${details.author_name}</p>
-            <p>Publisher Name: ${details.publisher}</p>
-            <p>Published year: ${details.first_publish_year}</p>
-        </div>
+    // Error Handing
+    if (convertedArray.length === 0) {
+        errorDiv.innerText = "NO Result Found";
+    } else {
+        errorDiv.innerText = "";
+    }
+    const slicedArrry = convertedArray.slice(0, 12);
+    slicedArrry.forEach(details => {
+        let imageUrl = `https://covers.openlibrary.org/b/id/${details.cover_i}-M.jpg`;
+        if(details.cover_i===undefined){
+           imageUrl = 'https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg';
+        }
+        const columnDiv = document.createElement('div');
+        columnDiv.className = 'col-md-3 col-12';
+        columnDiv.innerHTML = `
+            <div class="card p-3 my-2 shadow-lg p-3 mb-5 bg-white rounded" style="width: 18rem;">
+                <img id="notShown" src="${imageUrl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Book Name: ${details.title}</h5>
+                    <p class="card-text">Author Name: ${details.author_name}</p>
+                    <p class="card-text">Publisher Name: ${details.publisher}</p>
+                    <p class="card-text">Published year: ${details.first_publish_year}</p>
+                </div>
+            </div>
         `;
-        detailsContainer.appendChild(rowDiv);
+        detailsContainer.appendChild(columnDiv);
         count++;
+        countContainer.innerHTML = `Element: ${count}`;
     });
-    countContainer.innerHTML = `Element: ${count}`;
+    searchInput.value = '';
 };
